92ea3181291ca665643aa0ef9ee4e8b9ef47f57a
Olya7me <gumenyucko@yandex.ru>
2025-10-30 13:45:52 +0500

    fix: support screen rotation in PinModal

--- src/components/PinModal.tsx
+++ src/components/PinModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import ImageViewing from 'react-native-image-viewing';
+import ImageViewer from 'react-native-image-zoom-viewer';
 import { IconButton } from '../components';
 
 interface PinModalProps {
@@ -10,24 +11,36 @@ interface PinModalProps {
 }
 
 const PinModal: React.FC<PinModalProps> = ({ visible, imageUri, onClose }) => {
+	if (!visible) return null;
 	const insets = useSafeAreaInsets();
 
 	return (
-		<ImageViewing
-			images={[{ uri: imageUri }]}
-			imageIndex={0}
-			visible={visible}
-			onRequestClose={onClose}
-			HeaderComponent={() => (
-				<IconButton
-					iconName="close"
-					onPress={onClose}
-					style={{ top: insets.top + 10 }}
-				/>
-			)}
-			animationType="fade"
-		/>
+		<View style={styles.overlay}>
+			<ImageViewer
+				imageUrls={[{ url: imageUri }]}
+				enableSwipeDown
+				onSwipeDown={onClose}
+				renderIndicator={() => <View />}
+				renderHeader={() => (
+					<IconButton
+						iconName="close"
+						onPress={onClose}
+						style={{ top: insets.top + 10 }}
+					/>
+				)}
+				enablePreload
+				saveToLocalByLongPress={false}
+			/>
+		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	overlay: {
+		...StyleSheet.absoluteFillObject,
+		backgroundColor: 'black',
+		zIndex: 999,
+	},
+});
 
 export default PinModal;
